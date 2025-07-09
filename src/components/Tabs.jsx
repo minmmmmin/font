77db2714fe8eef8dc9676feae001ab371b13d2aa
minmmmmin <kotomi.hinahima@gmail.com>
2025-07-10 05:24:18 +0900

    スクロールがちょっとうざい

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -13,20 +13,20 @@ const tabList = [
 
 const Tabs = ({ font }) => {
   return (
-    <div className="space-y-8">
-      <section>
+    <div className="grid grid-cols-2 grid-rows-2 gap-8 p-4">
+      <section className="bg-white p-4 rounded shadow">
         <h2 className="text-xl font-semibold mb-2">Views</h2>
         <ViewsGraph data={font} />
       </section>
-      <section>
+      <section className="bg-white p-4 rounded shadow">
         <h2 className="text-xl font-semibold mb-2">OS Share</h2>
         <OsShareGraph data={font} />
       </section>
-      <section>
+      <section className="bg-white p-4 rounded shadow">
         <h2 className="text-xl font-semibold mb-2">Browser Share</h2>
         <BrowserShareGraph data={font} />
       </section>
-      <section>
+      <section className="bg-white p-4 rounded shadow">
         <h2 className="text-xl font-semibold mb-2">Trend</h2>
         <TrendGraph data={font} />
       </section>
