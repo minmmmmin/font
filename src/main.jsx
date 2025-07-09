6e2a597fd64a062f11c1bb31dd479ee92f065fbc
minmmmmin <kotomi.hinahima@gmail.com>
2025-07-10 01:44:43 +0900

    cssのあれこれ

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "../style.css";
 createRoot(document.querySelector("#content")).render(<App />);
