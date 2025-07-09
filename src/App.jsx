9fde4259bcadd5633890e43e9ce9cbff280096ee
minmmmmin <kotomi.hinahima@gmail.com>
2025-07-10 03:54:37 +0900

    ディレクトリだけ量産

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Dashboard from "./pages/Dashboard";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Header></Header>
-      <Footer></Footer>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow p-4 max-w-6xl mx-auto">
+        <Dashboard />
+      </main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+const Dashboard = () => {
+  return (
+    null
+  );
+};
+
+export default Dashboard;
